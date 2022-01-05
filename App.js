7dbe9e7971ae2acfcfa24fb2314c8fd3b8e7edd7
steve-ux <exe.universal@gmail.com>
2022-01-04 21:16:41 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esta es la home de MyTinerary</Text>
       <StatusBar style="auto" />
     </View>
   );
